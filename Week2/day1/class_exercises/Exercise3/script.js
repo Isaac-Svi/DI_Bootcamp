3adf99c9c03ad1692931762b4047c939a7e65e04
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-04 13:17:33 +0300

    exercise 3 day 1 week 2

---
+++ Week2/day1/class_exercises/Exercise3/script.js
@@ -0,0 +1,13 @@
+const pets = ['cat', 'dog', 'fish', 'rabbit', 'cow']
+
+// display dog
+console.log(pets[1])
+
+// add horse to end of array
+pets.push('horse')
+
+// remove rabbit
+pets.splice(3, 1)
+
+// display array length
+console.log(pets.length)
