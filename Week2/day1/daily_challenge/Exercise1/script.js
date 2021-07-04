bfd767f1478d38c174f09a19aa7bb0851778d2f8
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-04 13:18:33 +0300

    daily challenge week 2 day 1 exercise 1

---
+++ Week2/day1/daily_challenge/Exercise1/script.js
@@ -0,0 +1,32 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// removing Banana using shift since we know it's at the beginning
+fruits.shift()
+
+// sort array in alphabetical order
+// fruits.sort((a, b) => {
+//   a = a.toLowerCase()
+//   b = b.toLowerCase()
+
+//   return a.charCodeAt(0) - b.charCodeAt(0)
+// })
+
+// unnecessary to do the above, because that's already
+// the default behavior of sort
+fruits.sort()
+
+// add Kiwi to end of the array
+fruits.push('Kiwi')
+
+// remove Apples from the array
+
+// first find index of Apples
+const appleIndex = fruits.findIndex(x => x === 'Apples')
+
+// now remove Apples
+fruits.splice(appleIndex, 1)
+
+// reverse array
+fruits.reverse()
+
+console.log(fruits)
