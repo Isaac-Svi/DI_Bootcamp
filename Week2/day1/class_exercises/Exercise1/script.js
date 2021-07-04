580273d131a7030ade3300cc0082948cf79effa0
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-04 13:16:24 +0300

    exercise 1 day 1 week 2

---
+++ Week2/day1/class_exercises/Exercise1/script.js
@@ -0,0 +1,7 @@
+let addressNumber = 5,
+    addressStreet = "Main St.",
+    country = 'USA'
+
+let globalAddress = `Bob lives in ${addressStreet} ${addressNumber}, ${country}`
+
+console.log(globalAddress)
