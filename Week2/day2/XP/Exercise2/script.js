79a9405863c2f5eee8eed540a94582764db753fa
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-05 13:11:24 +0300

    week 2 day 2 xp exercise 2

---
+++ Week2/day2/XP/Exercise2/script.js
@@ -0,0 +1,17 @@
+// 1
+let newDog = 'Chihuahua'
+
+// 2
+let newDogLength = newDog.length
+console.log(newDogLength)
+
+// 3
+console.log(newDog.toUpperCase())
+console.log(newDog.toLowerCase())
+
+// 4
+if (newDog === 'Chihuahua') {
+  console.log('I love Chihuahuas, it’s my favorite dog breed')
+} else {
+  console.log('I dont care, I prefer cats')
+}
