c217dc51f94b808bd7c59684c6123a1603531221
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-05 13:17:55 +0300

    week 2 day 2 class exercises objects exercise 1

---
+++ Week2/day2/class_exercises/Objects/Exercise1/script.js
@@ -0,0 +1,24 @@
+// 1)
+const user = {
+  username: 'Bob',
+  password: 'test1234',
+}
+
+// 2)
+const database = [user]
+
+// 3)
+const newsfeed = [
+  {
+    username: 'Bob',
+    timeline: Date.now(),
+  },
+  {
+    username: 'John',
+    timeline: Date.now(),
+  },
+  {
+    username: 'Jerry',
+    timeline: Date.now(),
+  },
+]
