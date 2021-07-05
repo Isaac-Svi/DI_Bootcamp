a88db7c385747be41037247b6e381e98c5b95e43
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-05 13:13:43 +0300

    week 2 day 2 xp gold exercise 2

---
+++ Week2/day2/XP Gold/Exercise2/script.js
@@ -0,0 +1,11 @@
+const grade = Number(prompt('What is your grade?'))
+
+if (grade < 70) {
+  alert('D')
+} else if (grade <= 80) {
+  alert('C')
+} else if (grade <= 90) {
+  alert('B')
+} else {
+  alert('A')
+}
