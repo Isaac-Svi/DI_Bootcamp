4f3429a04a12c9ca424f3342467592828be6c09f
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-06 14:50:53 +0300

    week 2 day 3 daily challenge gold

---
+++ Week2/day3/daily_challenge_gold/script.js
@@ -0,0 +1,25 @@
+const numbers = [5, 0, 9, 1, 7, 4, 2, 6, 3, 8]
+
+// 1
+const numbersString = numbers.toString()
+
+// 2
+const numbersString1 = numbers.join('+')
+const numbersString2 = numbers.join(',')
+const numbersString3 = numbers.join('😎')
+
+// 3
+const bubbleSortDesc = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 1; j < arr.length; j++) {
+      if (arr[j - 1] < arr[j]) {
+        const temp = arr[j]
+        arr[j] = arr[j - 1]
+        arr[j - 1] = temp
+      }
+    }
+  }
+}
+
+bubbleSortDesc(numbers)
+console.log(numbers)
