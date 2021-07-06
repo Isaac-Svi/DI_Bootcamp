12c0b890c592cc27e3b2801421c7c05b57bd04dd
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-06 16:53:57 +0300

    add console logs to daily challenge gold w2/d3

--- Week2/day3/daily_challenge_gold/script.js
+++ Week2/day3/daily_challenge_gold/script.js
@@ -2,11 +2,15 @@ const numbers = [5, 0, 9, 1, 7, 4, 2, 6, 3, 8]
 
 // 1
 const numbersString = numbers.toString()
+console.log(numbersString)
 
 // 2
 const numbersString1 = numbers.join('+')
 const numbersString2 = numbers.join(',')
 const numbersString3 = numbers.join('😎')
+console.log(numbersString1)
+console.log(numbersString2)
+console.log(numbersString3)
 
 // 3
 const bubbleSortDesc = (arr) => {
