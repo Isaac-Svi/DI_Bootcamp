49e6647208266e22b8abaf23f3cf43ec1fa5e579
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-07 16:01:49 +0300

    fixed bonus.js to use different prompts and no label

--- Week2/day4/XP/Exercise9/bonus.js
+++ Week2/day4/XP/Exercise9/bonus.js
@@ -28,29 +28,38 @@ const rentalCarCost = (numDays) => {
 }
 
 // 4
+const isValidNumber = (x) => {
+  return x && !isNaN(Number(x))
+}
+const isValidString = x => {
+  return x && isNaN(Number(x))
+}
+
 const totalVacationCost = () => {
-  let hotelNights, carRent, destination, input
-  const promptText = "Enter how many nights you'll be staying in the hotel, the number of days you'll be renting a car, and your destination, separated by commas (,): "
-  
-  main: while (true) {
-    input = prompt(promptText)
-    const [a, b, c] = input.split(',')
+  let hotelNights, carRentDuration, destination
+  let hotelPrompt = "Enter how many nights you'll be staying in the hotel",
+      carPrompt = "Enter the number of days you'll be renting a car",
+      destinationPrompt = "Enter your destination"
 
-    const parameterMissing = !a || !b || !c,
-          parameterWrongType = isNaN(Number(a)) || isNaN(Number(b)) || !isNaN(Number(c))
-
-    if (parameterMissing || parameterWrongType) {
-      alert("Some data provided was incorrect")
-      continue main
-    }
-
-    hotelNights = a
-    carRent = b
-    destination = c
+  while (true) {
+    hotelNights = prompt(hotelPrompt)
+    if (!isValidNumber(hotelNights)) continue
     break
   }
 
-  return (rentalCarCost(carRent) + hotelCost(hotelNights) + planeRideCost(destination)).toFixed(2)
+  while (true) {
+    carRentDuration = prompt(carPrompt)
+    if (!isValidNumber(carRentDuration)) continue
+    break
+  }
+
+  while (true) {
+    destination = prompt(destinationPrompt)
+    if (!isValidString(destination)) continue
+    break
+  }
+    
+  return (rentalCarCost(carRentDuration) + hotelCost(hotelNights) + planeRideCost(destination)).toFixed(2)
 }
 
 // 5
