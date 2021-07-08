ae3b51c51f696b951f372b2fd32d18c066d619f3
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-08 11:20:22 +0300

    prettify js file

--- Week2/day4/XP Ninja/Exercise4/script.js
+++ Week2/day4/XP Ninja/Exercise4/script.js
@@ -1,7 +1,7 @@
 const biggestNumberInArray = (arr) => {
   // get to first number
   let i = 0
-  while(i < arr.length && typeof arr[i++] !== 'number');
+  while (i < arr.length && typeof arr[i++] !== 'number');
 
   // this means no numbers were found
   if (i === arr.length) return 0
@@ -10,7 +10,7 @@ const biggestNumberInArray = (arr) => {
   let max = arr[i]
 
   // once first number is found, we can continue from there
-  for (;i < arr.length; i++) {
+  for (; i < arr.length; i++) {
     if (typeof arr[i] === 'number' && arr[i] > max) {
       max = arr[i]
     }
@@ -19,9 +19,9 @@ const biggestNumberInArray = (arr) => {
   return max
 }
 
-const array = [-1,0,3,100, 99, 2, 99] ;// should return 100 
-const array2 = ['a', 3, 4, 2]; // should return 4 
-const array3 = []; // should return 0
+const array = [-1, 0, 3, 100, 99, 2, 99] // should return 100
+const array2 = ['a', 3, 4, 2] // should return 4
+const array3 = [] // should return 0
 
 console.log(biggestNumberInArray(array))
 console.log(biggestNumberInArray(array2))
