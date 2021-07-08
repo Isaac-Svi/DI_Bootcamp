ade93c7f5e95fc194e9692a088207ab23c47d439
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-08 13:57:46 +0300

    week 2 day 5 daily_challenge/

---
+++ Week2/day5/daily_challenge/script.js
@@ -0,0 +1,23 @@
+// 1
+let numBottles = Number(prompt('Enter a number: '))
+console.log(numBottles)
+
+// 2
+let decrementor = 1
+
+while (numBottles > 0) {
+  let s = numBottles === 1 ? '' : 's'
+  console.log(`${numBottles} bottle${s} of beer on the wall`)
+  console.log(`${numBottles} bottle${s} of beer`)
+
+  // make sure decrementor doesn't make numBottles < 0
+  decrementor = numBottles - decrementor < 0 ? numBottles : decrementor
+  const itOrThem = decrementor === 1 ? 'it' : 'them'
+  console.log(`Take ${decrementor} down, pass ${itOrThem} around`)
+
+  numBottles -= decrementor++
+  s = numBottles === 1 ? '' : 's'
+  console.log(`${numBottles} bottle${s} of beer on the wall`)
+
+  console.log('---------------------------------')
+}
