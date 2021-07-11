63cba7539459ef6a3f10477e9f9de91a2cc849a9
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-11 15:36:18 +0300

    week 3 day 1 class exercise 4

---
+++ Week3/day1/class_exercises/Exercise4/script.js
@@ -0,0 +1,6 @@
+const text = document.getElementById('text')
+
+text.style.fontSize = '25px'
+text.style.fontFamily = 'monospace'
+text.style.color = 'turquoise'
+text.style.backgroundColor = 'slategray'
