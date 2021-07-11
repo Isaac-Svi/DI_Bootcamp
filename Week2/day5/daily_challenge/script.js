ea886cad29b2ab8f2f569e1d2410a94b0fd511aa
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-11 10:10:39 +0300

    add check to make sure user enters number and remove unnecessary Number conversion

--- Week2/day5/daily_challenge/script.js
+++ Week2/day5/daily_challenge/script.js
@@ -1,22 +1,25 @@
 // 1
-let numBottles = Number(prompt('Enter a number: '))
+let numBottles
+while (isNaN(numBottles)) {
+    numBottles = prompt('Enter a number: ')
+}
 
 // 2
 let decrementor = 1
 
 while (numBottles > 0) {
-  let s = numBottles === 1 ? '' : 's'
-  console.log(`${numBottles} bottle${s} of beer on the wall`)
-  console.log(`${numBottles} bottle${s} of beer`)
+    let s = numBottles === 1 ? '' : 's'
+    console.log(`${numBottles} bottle${s} of beer on the wall`)
+    console.log(`${numBottles} bottle${s} of beer`)
 
-  // make sure decrementor doesn't make numBottles < 0
-  decrementor = numBottles - decrementor < 0 ? numBottles : decrementor
-  const itOrThem = decrementor === 1 ? 'it' : 'them'
-  console.log(`Take ${decrementor} down, pass ${itOrThem} around`)
+    // make sure decrementor doesn't make numBottles < 0
+    decrementor = numBottles - decrementor < 0 ? numBottles : decrementor
+    const itOrThem = decrementor === 1 ? 'it' : 'them'
+    console.log(`Take ${decrementor} down, pass ${itOrThem} around`)
 
-  numBottles -= decrementor++
-  s = numBottles === 1 ? '' : 's'
-  console.log(`${numBottles} bottle${s} of beer on the wall`)
+    numBottles -= decrementor++
+    s = numBottles === 1 ? '' : 's'
+    console.log(`${numBottles} bottle${s} of beer on the wall`)
 
-  console.log('---------------------------------')
+    console.log('---------------------------------')
 }
