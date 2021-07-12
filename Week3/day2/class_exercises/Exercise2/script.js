101d8e9e3ae52f0bc2f9229f4656f67650255aa6
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-12 16:04:06 +0300

    week 3 day 2 class exercises Exercise2/

---
+++ Week3/day2/class_exercises/Exercise2/script.js
@@ -0,0 +1,27 @@
+const styleChanger = document.getElementById('jsstyle')
+
+const getRandomColor = () => `#${(((1 << 24) * Math.random()) | 0).toString(16)}`
+
+const handleClick = ({ target }) => {
+    target.style.backgroundColor = getRandomColor()
+}
+const handleMouseOver = ({ target }) => {
+    target.style.color = getRandomColor()
+}
+let toggle = true
+const handleDoubleClick = ({ target }) => {
+    target.style.transform = `scale(${toggle ? 1.2 : 1})`
+    toggle = !toggle
+}
+const handleMouseDown = ({ target }) => {
+    target.style.transform = 'scale(1.2)'
+}
+const handleMouseUp = ({ target }) => {
+    target.style.transform = 'scale(1)'
+}
+
+styleChanger.addEventListener('click', handleClick)
+styleChanger.addEventListener('mouseover', handleMouseOver)
+styleChanger.addEventListener('dblclick', handleDoubleClick)
+styleChanger.addEventListener('mousedown', handleMouseDown)
+styleChanger.addEventListener('mouseup', handleMouseUp)
