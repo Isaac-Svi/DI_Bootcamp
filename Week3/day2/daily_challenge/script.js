6b6807ee12a319eb8cd7748417d62449fe049c5f
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-12 16:33:03 +0300

    week 3 day 2 daily challenge

---
+++ Week3/day2/daily_challenge/script.js
@@ -0,0 +1,53 @@
+// 1
+const inputs = [...document.querySelectorAll('input')]
+const libButton = document.getElementById('lib-button')
+const shuffleButton = document.getElementById('shuffle')
+
+const getValues = () => {
+    const values = []
+
+    for (const { value } of inputs) {
+        // 2
+        if (!value) return []
+
+        values.push(value)
+    }
+
+    return values
+}
+
+// 3, 4, 5
+const storyGenerator = () => {
+    let index = 0
+    const stories = []
+
+    const obj = {
+        shuffle() {
+            document.getElementById('story').innerText = stories[index]
+            index = index === stories.length - 1 ? 0 : ++index
+        },
+        generateStory() {
+            const values = getValues()
+            if (!values.length) {
+                alert('Please fill in all fields')
+                return
+            }
+
+            const [noun, adjective, name, verb, place] = values
+
+            stories.push(
+                `There once was a ${adjective} ${noun} that ${verb} all of ${name}'s food at ${place}.`,
+                `Watching ${name} ${verb}ing all the ${adjective} ${noun}s at ${place} was quite a site.`,
+                `I saw ${name}'s ${adjective} ${noun} ${verb}ing in ${place}.`
+            )
+
+            obj.shuffle()
+        },
+    }
+
+    return obj
+}
+
+const { generateStory, shuffle } = storyGenerator()
+libButton.addEventListener('click', generateStory)
+shuffleButton.addEventListener('click', shuffle)
