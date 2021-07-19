44a74b8bbbb9f7b0a24650a200ed1164be89519c
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-19 10:49:33 +0300

    week 4 day 1 xp Exercise2/

---
+++ Week4/day1/XP/Exercise2/script.js
@@ -0,0 +1,17 @@
+function winBattle() {
+    return true
+}
+
+// function experiencePoints() {
+//     if (winBattle()) {
+//         return 10;
+//     } else {
+//         return 1;
+//     }
+// }
+
+// experiencePoints()
+
+// Answer:
+
+const experiencePoints = winBattle() ? 10 : 1
