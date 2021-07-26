66a00503f7429a7ea2d2484f6c2f35ece8976eeb
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-26 16:21:16 +0300

    week 5 day 2 daily challenge

---
+++ Week5/day2/daily_challenge/script.js
@@ -0,0 +1,60 @@
+HTMLElement.prototype.appendMany = function () {
+    for (const child of arguments) {
+        this.append(child)
+    }
+}
+
+const createElement = (tag, text = '', attributes) => {
+    const el = document.createElement(tag)
+
+    el.innerText = text
+
+    for (const a in attributes) {
+        el.setAttribute(a, attributes[a])
+    }
+
+    return el
+}
+
+const output = document.querySelector('.output')
+
+document.forms[0].addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const API_URL =
+        'https://api.giphy.com/v1/gifs/search?' +
+        new URLSearchParams({
+            q: e.target.search.value,
+            limit: 1,
+            rating: 'g',
+            api_key: 'hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My',
+        }).toString()
+
+    const xhr = new XMLHttpRequest()
+
+    xhr.open('GET', API_URL)
+
+    xhr.responseType = 'json'
+
+    xhr.send()
+
+    xhr.addEventListener('load', () => {
+        const { url, height } = xhr.response.data[0].images.downsized
+
+        const imgContainer = createElement('div', '', { class: 'img-container' })
+
+        imgContainer.appendMany(
+            createElement('img', '', { src: url, height }),
+            createElement('p', e.target.name.value),
+            createElement('button', 'Delete', {
+                onclick: `(event => event.target.parentNode.remove())(event)`,
+            })
+        )
+
+        output.append(imgContainer)
+
+        e.target.reset()
+    })
+})
+
+document.getElementById('delete').addEventListener('click', () => (output.innerHTML = ''))
