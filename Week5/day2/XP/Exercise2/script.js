2d00d0a2ef3dddc27e4eeb6ab88230e8291d36ad
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-26 12:45:59 +0300

    add limit

--- Week5/day2/XP/Exercise2/script.js
+++ Week5/day2/XP/Exercise2/script.js
@@ -2,7 +2,7 @@ const xhr = new XMLHttpRequest()
 
 xhr.open(
     'GET',
-    'https://api.giphy.com/v1/gifs/search?q=sun&offset=2&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My'
+    'https://api.giphy.com/v1/gifs/search?q=sun&limit=10&offset=2&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My'
 )
 
 xhr.responseType = 'json'
