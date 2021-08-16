2c632f65d0d5f6517dd19244f7720e5cf97d1eaf
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-08-16 11:00:27 +0300

    add XP.js as entry point for all xp exercises

---
+++ Week8/day1/src/XP/XP.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Exercise1 from './Exercise1'
+import Exercise2 from './Exercise2'
+import Exercise3 from './Exercise3'
+import Exercise4 from './Exercise4'
+import Exercise5 from './Exercise5'
+import Exercise6 from './Exercise6'
+import Exercise7 from './Exercise7'
+import Exercise7_2 from './Exercise7.2'
+
+const XP = () => {
+    return (
+        <div>
+            <Exercise1 />
+            <Exercise2 />
+            <Exercise3 />
+            <Exercise4 />
+            <Exercise5 />
+            <Exercise6 />
+            <Exercise7 />
+            <Exercise7_2 />
+        </div>
+    )
+}
+
+export default XP
