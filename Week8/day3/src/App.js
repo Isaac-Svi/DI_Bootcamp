ffaf3b028e6894176ca9a75dbd3b561114d2e660
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-08-18 00:54:21 +0300

    add rest of week 8 day 3 files

---
+++ Week8/day3/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './style.css'
+// import XP from './XP/XP'
+// import XPP from './XPP/XPP'
+// import DailyChallenge from './daily_challenge/DailyChallenge'
+// import XP_Gold from './XP_Gold/XP_Gold'
+// import Clock from './MiniProject-Clock/Clock'
+import Form from './MiniProject-FormValidation/Form'
+
+const App = () => {
+    return (
+        <>
+            {/* <XP /> */}
+            {/* <XPP /> */}
+            {/* <DailyChallenge /> */}
+            {/* <XP_Gold /> */}
+            {/* <Clock /> */}
+            <Form />
+        </>
+    )
+}
+
+export default App
