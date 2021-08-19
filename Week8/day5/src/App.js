cb75165c98c52b1e0df6001849081b7eef49fc83
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-08-19 16:01:49 +0300

    week 8 day 5 other files

---
+++ Week8/day5/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+// import Quote from './MiniProject-QuoteGenerator/Quote'
+// import TodoList from './MiniProject-TodoList/TodoList'
+import Game from './daily_challenge/Game'
+
+const App = () => {
+    return (
+        <>
+            {/* <Quote /> */}
+            {/* <TodoList /> */}
+            <Game />
+        </>
+    )
+}
+
+export default App
