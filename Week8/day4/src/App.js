1461895f800cec57433757abbebd8b7fbc19e936
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-08-20 02:24:13 +0300

    rest of files week 8 day 5

--- Week8/day4/src/App.js
+++ Week8/day4/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './style.css'
 // import XP from './XP/XP'
-import XPN from './XP_Ninja-ErrorBoundary/XPN'
+// import XPN from './XP_Ninja-ErrorBoundary/XPN'
+import Clock from './MiniProject-ReactClock/Clock'
 
 const App = () => {
     return (
         <>
             {/* <XP /> */}
-            <XPN />
+            {/* <XPN /> */}
+            <Clock />
         </>
     )
 }
