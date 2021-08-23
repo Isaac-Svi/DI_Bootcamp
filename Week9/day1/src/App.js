afb081f728a5409667bff9ec6f4dc1f59b3b3fab
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-08-23 16:48:19 +0300

    other files

---
+++ Week9/day1/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './style.css'
+// import Exercise1 from './XP/Exercise1'
+// import Exercise2 from './XP/Exercise2'
+// import Exercise3 from './XP/Exercise3/Exercise3'
+// import PostList from './XPP/Exercise1/PostList'
+// import Example1 from './XPP/Exercise2/Example1'
+// import Example2 from './XPP/Exercise2/Example2'
+// import Example3 from './XPP/Exercise2/Example3'
+// import FetchButton from './XPP/Exercise3/FetchButton'
+// import AutoCompletedText from './daily_challenge/AutoCompletedText'
+// import Client from './daily_challenge_2/Client'
+// import MiniProject from './MiniProject-Users&Posts/MiniProject'
+// import XPG from './XP_Gold/XPG'
+// import XPN from './XP_Ninja/Exercise1/Client/XPN'
+import Customers from './XP_Ninja/Exercise2/components/Customers'
+
+const App = () => {
+    return (
+        <>
+            {/* <Exercise1 /> */}
+            {/* <Exercise2 /> */}
+            {/* <Exercise3 /> */}
+            {/* <PostList /> */}
+            {/* <Example1 /> */}
+            {/* <Example2 /> */}
+            {/* <Example3 /> */}
+            {/* <FetchButton /> */}
+            {/* <AutoCompletedText /> */}
+            {/* <Client /> */}
+            {/* <MiniProject /> */}
+            {/* <XPG /> */}
+            {/* <XPN /> */}
+            <Customers />
+        </>
+    )
+}
+
+export default App
