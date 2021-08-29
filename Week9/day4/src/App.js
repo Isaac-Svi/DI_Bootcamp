7c1eaac8e430174565435f78bed78ce61159568b
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-08-29 20:17:34 +0300

    Extra files week 9 day 4

---
+++ Week9/day4/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import DC from './daily_challenge/DC'
+import './style.css'
+// import XP from './XP/XP'
+
+const App = () => {
+    return (
+        <>
+            {/* <XP /> */}
+            <DC />
+        </>
+    )
+}
+
+export default App
