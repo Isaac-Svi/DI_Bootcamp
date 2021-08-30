86649c1c33bc7ed71c7815aeafedbbf15700039c
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-08-30 17:58:36 +0300

    week 9 day 5 other files

---
+++ Week9/day5/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DC from './daily_challenge/DC'
+// import ReduxBlog from './MiniProject-ReduxBlog/ReduxBlog'
+
+const App = () => {
+    return (
+        <>
+            {/* <ReduxBlog /> */}
+            <DC />
+        </>
+    )
+}
+
+export default App
--- Week9/day5/src/MiniProject-ReduxBlog/ReduxBlog.js
+++ Week9/day5/src/MiniProject-ReduxBlog/ReduxBlog.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -7,10 +8,18 @@ import Navbar from './components/Navbar'
 import Post from './components/Post'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import './style.css'
 
 const MiniProject = () => {
     return (
         <Provider store={store}>
+            {/* adding style link like this, so i don't have to add it to index.html */}
+            <Helmet>
+                <link
+                    href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css'
+                    rel='stylesheet'
+                />
+            </Helmet>
             <Navbar />
             <Switch>
                 <Route path='/' exact component={Home} />
