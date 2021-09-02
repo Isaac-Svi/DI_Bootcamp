dd10ef50ea0b64748593168b94904715250e95e4
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-09-02 03:53:31 +0300

    week 9 other files

--- Week9/day5/src/App.js
+++ Week9/day5/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import DC from './daily_challenge/DC'
+import ReminderApp from './MiniProject-ReminderApp/ReminderApp'
+// import DC from './daily_challenge/DC'
 // import ReduxBlog from './MiniProject-ReduxBlog/ReduxBlog'
 
 const App = () => {
     return (
         <>
             {/* <ReduxBlog /> */}
-            <DC />
+            {/* <DC /> */}
+            <ReminderApp />
         </>
     )
 }
